c4183793ec654c8c97a109b044f02d8ffee4ca71
이세은 <senyvee9@gmail.com>
2024-02-13 17:54:36 +0900

    feat: 프론트에서 byte[] 전달 방식으로 변경

--- BackEnd/src/main/java/com/ssafy/backend/domain/stt/controller/SttController.java
+++ BackEnd/src/main/java/com/ssafy/backend/domain/stt/controller/SttController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.sound.sampled.LineUnavailableException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 @Slf4j
@@ -23,19 +24,23 @@ public class SttController {
     private final SttService sttService;
 
     @MessageMapping("/api/v1/audio") //websocket에서 제공하는 vue에서 메시지 받을 컨트롤러
-    public void SttHandler(String base64Audio) throws LineUnavailableException, IOException {
-        Base64.Decoder decoder = Base64.getDecoder();
-        StringBuilder sb = new StringBuilder();
+    public void SttHandler(@Payload byte[] audioData) throws LineUnavailableException, IOException {
 
-//        byte[] decoded = decoder.decode(base64Audio.split(",")[1]);
+        System.out.println("호출!");
+        log.info("오디오데이터 들어옴: " + audioData.length + "bytes");
+
+
+//        Base64.Decoder decoder = Base64.getDecoder();
+//        StringBuilder sb = new StringBuilder();
+//
+//        byte[] decoded = decoder.decode(audioData.split(",")[1]);
 //        sb.append(new String(decoded, StandardCharsets.UTF_8));
 //        log.info("오디오데이터 들어옴: " + sb);
-//        String dummy = "data:audio/webm;base64,GkXfo59ChoEBQveBAULygQRC84EIQoKEd2VibUKHgQRChYECGFOAZwH/////////FUmpZpkq17GDD0JATYCGQ2hyb21lV0GGQ2hyb21lFlSua7+uvdeBAXPFh2kUz9yhnp6DgQKGhkFfT1BVU2Oik09wdXNIZWFkAQEAAIC7AAAAAADhjbWERzuAAJ+BAWJkgSAfQ7Z1Af/////////ngQCjRLWBAACA+4P9Qf0JfcWdkxOG5X3hjLyYhlGNVHUn9D8B1PHsvlJSJR5eLKmP8cdgrF1Fb66T/pGRlewx9DyJKsMJAblqTJwoUrH7AMrcGkGc0swDP38zlOd32hD5xpr7xxu0Xp4xkne97kRAslXuZYNFf9yZBfxe9hfR2BPS2WYgYQ7lwt/hngQWekkzgwv2NjWDokfcOTsCU6HNR2Tiw+Z5d61CS+Wg";
-//        byte[] decoded = decoder.decode(dummy.split(",")[1]);
 //        FileOutputStream fos = new FileOutputStream("MyAudio.webm");
 //        fos.write(decoded);
 //        fos.close();
     }
+
 //    @PostMapping("/stt")
 //    public void startStt() throws LineUnavailableException, IOException {
 //        sttService.startStt();
